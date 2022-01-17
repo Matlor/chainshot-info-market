7c41813b8324a9e42045884ec87eaf785cb7511b
mathias lorenceau <mathiaslorenceau@mathiass-MacBook-Pro.local>
2022-01-17 17:33:19 +0100

    deleted console.log

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,7 @@ const account3 = process.env.ACCOUNT_3;
 const account4 = process.env.ACCOUNT_4;
 
 const rinkebyEndpoint = process.env.RINKEBY_ENDPOINT;
-console.log(account1)
-console.log(typeof account1)
-console.log(rinkebyEndpoint)
+
 
 module.exports = {
 	solidity: "0.8.4",
